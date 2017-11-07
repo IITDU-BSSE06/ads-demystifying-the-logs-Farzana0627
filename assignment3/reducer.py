9e2053539c0d5f580ad0d1703ce35d754c95a5cc
Farzana0627 <sinceramuna@gmail.com>
2017-11-07 14:20:21 +0600

    Reducer for assignment 3
    Reads each line containing the file paths one by one and finds the file with maximum hits.

--- assignment3/reducer.py
+++ assignment3/reducer.py
@@ -4,17 +4,19 @@ import sys
 
 Total = 0
 oldKey = None
-Max = 0
-maxhitpath = None
+Max=0
+maxhitpath= None
 
 for line in sys.stdin:
     data_mapped = line.strip()
+    if data_mapped == "" or data_mapped is None :
+	continue
     thisKey = data_mapped
 
     if oldKey and oldKey != thisKey:
-        if Total>Max:
-		        Max = Total
-		        maxhitpath = oldKey
+        if Total > Max:
+		Max = Total
+		maxhitpath= oldKey
         oldKey = thisKey;
         Total = 0
 
@@ -22,3 +24,4 @@ for line in sys.stdin:
     Total = Total+1
 
 print maxhitpath
+

699902f8f1de115259acc4b0a950d90703905559
Farzana0627 <sinceramuna@gmail.com>
2017-11-07 14:05:22 +0600

    Update reducer.py
    Counts the total number of times the IP address "10.99.99.186" was hit.

--- assignment1/reducer.py
+++ assignment1/reducer.py
@@ -4,9 +4,10 @@ import sys
 
 Total = 0
 
+
 for line in sys.stdin:
     data = line.strip()
-    if data == "":
+    if data == "" or data is None:
 	continue
     if data == "10.99.99.186":
 	 Total=Total+1
